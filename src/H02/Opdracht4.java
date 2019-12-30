7fdf21ae1b62169659aae0323181eea7320edcfe
MarkBout <mbout@roc-dev.com>
2019-12-30 17:29:51 +0100

    Opdracht 4

--- src/H02/Opdracht4.java
+++ src/H02/Opdracht4.java
@@ -1,7 +1,12 @@
 package H02;
 import java.util.Scanner;
+
 public class Opdracht4 {
     public static void main(String[] args) {
         Scanner invoer = new Scanner(System.in);
+
+        System.out.print("Voer een getal heel in: ");
+        int userinput = invoer.nextInt();
+        System.out.println("De vijf getallen zijn: " + (userinput - 2) + " " + (userinput - 1) + " " + userinput + " " + (userinput + 1) + " " + (userinput + 2));
     }
 }
