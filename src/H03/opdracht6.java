920a7c3f32ea4486778bb87f4d2f1685116b85a4
MarkBout <mbout.roc-dev.com>
2020-01-07 19:27:37 +0100

    H03 Opdracht 6

---
+++ src/H03/opdracht6.java
@@ -0,0 +1,13 @@
+package H03;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class opdracht6 {
+    public static Scanner invoer = new Scanner(System.in).useLocale(Locale.US);
+    public static double getal1, getal2, getal3;
+
+    public static void main(String[] args) {
+
+    }
+}
